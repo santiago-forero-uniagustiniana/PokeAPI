029ca32273c681b4b1eed4ac775bf5050efba60e
santiago-forero-uniagustiniana <santiago.forerob@uniagustiniana.edu.co>
2026-09-24 15:12:43 +0000

    PokeAPI v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
+import {BrowserRouter as Router, Route, Routes, Navigate, Link} from 'react-router-dom';
 import Coleccion from "./componentes/coleccion"
 import Favoritos from "./componentes/favoritos"
 import Info from "./componentes/info"
 import Inicio from "./componentes/inicio"
-import Pokemon from "./componentes/pokemon"
 import Usuario from "./componentes/usuario"
+import "./App.css"
+
+import Pokemon from "./componentes/pokemon"
 
 function App() {
     
   return (
       <>
-        <Inicio />        
-        <Coleccion />
-        <Favoritos />
-        <Info />
-        <Usuario />
-        <Pokemon />
+      <Router>
+
+        <nav className='c-menu'>
+          <Link to="/">Inicio</Link>
+          <Link to="/coleccion">Coleccion</Link>
+          <Link to="/favoritos">Favoritos</Link>
+          <Link to="/info">Info</Link>
+          <Link to="/usuario">Usuario</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Inicio/>}/>
+          <Route path="/coleccion" element={<Coleccion/>}/>
+          <Route path="/favoritos" element={<Favoritos/>}/>
+          <Route path="/info" element={<Info/>}/>
+          <Route path="/usuario" element={<Usuario/>}/>
+           <Route path="/pokemon/:name" element={<Pokemon />} />
+        </Routes>
+      </Router>
       </>
   )
 }
--- src/componentes/pokemon/index.jsx
+++ src/componentes/pokemon/index.jsx
@@ -1,10 +1,25 @@
+import { useParams } from "react-router-dom"
+import { useState, useEffect } from 'react'
 import "./style.css"
 
 function Pokemon() {
-    
+    const {name} = useParams();
+    const [datapoke, setDatapoke] = useState([]);
+
+    useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      .then(response => response.json())
+      .then(responseData => setDatapoke(responseData))
+      .catch(error => console.error("Error:", error));
+    }, [name]); 
+    console.log(datapoke)
+
+    if (!datapoke || !datapoke.id) return <p>Cargando...</p>;
+     
     return (
       <>
-        Pokemon
+        {datapoke.name}
+        {datapoke.abilities[0].ability.name}
       </>
     )
 }
